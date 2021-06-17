d648e8977426709b4a79534f43c0b30e9195fab2
Zhe Kuan Lin <vincentlin19910619@gmail.com>
2021-06-17 15:12:16 +0800

    查詢檔案,判斷有無檔案後執行

--- product.py
+++ product.py
@@ -1,11 +1,30 @@
+# 確認有無檔案
+import os  # operating system
 products = []
-
+if os.path.isfile('product.csv'):
+    print('找到檔案')
+# 讀取檔案
+    with open('product.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue  # 跳過並繼續
+            name, price = line.strip().split(',')
+            products.append([name, price])
+else:
+    print('找不到檔案...')
+    exit()
+# 使用者輸入
 while True:
     name = input('請輸入商品：')
     if name == 'q':
         break
     price = input('請輸入價格：')
+    name = str(name)
+    price = int(price)
     products.append([name, price])
-with open('product.csv', 'w') as f:
+
+# 寫入檔案
+with open('product.csv', 'w', encoding='utf-8') as f:
+    f.write('商品,價格\n')
     for product in products:
-        f.write(product[0] + ',' + product[1] + '\n')
+        f.write(product[0] + ',' + str(product[1]) + '\n')
